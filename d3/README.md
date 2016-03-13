b9986826aba77901e577bf54fc8acafc43e576b0
jackzhao-mj <jackzhao.mj@gmail.com>
2016-03-12 16:12:28 -0800

    lineChart.

---
+++ d3/README.md
@@ -0,0 +1,4 @@
+/* README:
+ * Put lineChart.html, nv.d3.js, nv.d3.css in the same directory.
+ * Change 'url' in function: readJson to the path that .json file updates.
+ */ 
--- vyos-install.sh
+++ vyos-install.sh
@@ -140,3 +140,19 @@ virt-install --connect qemu:///system --ram 512 \
 --disk path=vyos-test-new.qcow2,device=disk,bus=virtio,format=qcow2 \
 --vcpus=2 --console pty,target_type=serial --noautoconsole --import \
 --graphics none
+
+
+qemu-system-x86_64 -enable-kvm -cpu host -m 1024M -smp 2,threads=1,sockets=1 
+-chardev socket,id=char0,path=/tmp/sn_vhost_$2 -netdev type=vhost-user,id=mynet0,chardev=char0,vhostforce 
+-device virtio-net-pci,netdev=mynet0,mac=52:54:00:02:d9:00 -chardev socket,id=char1,path=/tmp/sn_vhost_$3 
+-netdev type=vhost-user,id=mynet1,chardev=char1,vhostforce -device virtio-net-pci,netdev=mynet1,mac=52:54:00:02:d9:01 
+-object memory-backend-file,id=mem,size=1024M,mem-path=/mnt/huge,share=on -numa node,memdev=mem -mem-prealloc 
+-nographic $VM_INSTANCE.img 
+&> /var/log/$VM_INSTANCE.log &
+
+
+virt-install --connect qemu:///system --ram 512 \
+-n $VM_INSTANCE -r 1024 --os-type=linux --os-variant=generic \
+--disk path=$VM_INSTANCE.qcow2,device=disk,bus=virtio,format=qcow2 \
+--vcpus=2 --console pty,target_type=serial --noautoconsole --import \
+--graphics none
